d5c1c810239f070d435158c0c11efab71a533af8
Rishabh Saxena <orishabh1@gmail.com>
2016-04-06 20:22:42 +0530

    some minor changes

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 var linkCounter = 0;
-function frame(){
-	rtf.document.designMode = 'On';
+
+function frame() {
+    rtf.document.designMode = 'On';
 }
-function bold(){
-	rtf.document.execCommand('bold',false,null); 
+
+function bold() {
+    rtf.document.execCommand('bold', false, null);
 }
-function underline(){
-	rtf.document.execCommand('underline',false,null);
+
+function underline() {
+    rtf.document.execCommand('underline', false, null);
 }
-function link(){
-	var linkURL = prompt("Enter the URL for this link:", "http://"); 
-	rtf.document.execCommand("CreateLink", false, linkURL);
-	if (linkCounter%2 == 0)
-		$('<li> <a href="' + linkURL + '">'+ linkURL +'</a></li>').appendTo('#links');
-	else
-		$('<li> <a style="color:red;" href="' + linkURL + '">'+ linkURL +'</a></li>').appendTo('#links');
-	linkCounter += 1;
+
+function link() {
+    var linkURL = prompt("Enter the URL for this link:", "http://");
+    rtf.document.execCommand("CreateLink", false, linkURL);
+    if (linkCounter % 2 == 0)
+        $('<li> <a href="' + linkURL + '">' + linkURL + '</a></li>').appendTo('#links');
+    else
+        $('<li> <a style="color:red;" href="' + linkURL + '">' + linkURL + '</a></li>').appendTo('#links');
+    linkCounter += 1;
 }
-function foreColor(){
-	rtf.document.execCommand('ForeColor',false,'ff0000');
+
+function foreColor() {
+    rtf.document.execCommand('ForeColor', false, 'ff0000');
 }
-function submitForm(){
-	var theForm = document.getElementById("editor");
-	theForm.elements["textArea"].value = window.frames['rtf'].document.body.innerHTML;
-	theForm.submit();
+
+function submitForm() {
+    var theForm = document.getElementById("editor");
+    theForm.elements["textArea"].value = window.frames['rtf'].document.body.innerHTML;
+    theForm.submit();
 }
+
 // entering a new paragraph on every newline
 rtf.addEventListener('keypress', function(e) {
     if (e.keyCode == '13') {
@@ -33,3 +40,5 @@ rtf.addEventListener('keypress', function(e) {
         rtf.document.execCommand('formatBlock', false, 'p');
     }
 }, false);
+
+//draggable and droppable paragraphs
